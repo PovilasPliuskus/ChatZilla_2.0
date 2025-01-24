d40a1e7831c9ae7eae0e7debcd7af7718e77e7a7
Povilas Pliuskus <PovilasPliuskus@gmail.com>
2025-01-24 22:44:17 +0200

    Add main ChatZilla mascot
    Add the ChatZilla image to /assets/img/ directory, implemented it to the
    MainPage.vue file and add styles for it inside style.css file.

--- chatApp.client/src/components/MainPage.vue
+++ chatApp.client/src/components/MainPage.vue
@@ -1,3 +1,8 @@
 <template>
   <div id="main-page-heading">ChatZilla</div>
+  <img
+    src="../assets/img/ChatZilla-main-image.jpg"
+    alt="ChatZilla main dinosaur image"
+    class="header-image"
+  />
 </template>
