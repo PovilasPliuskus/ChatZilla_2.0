565931be7ca9deaeec774eb50f665974db539304
Povilas Pliuskus <PovilasPliuskus@gmail.com>
2025-01-25 14:27:31 +0200

    Add simple Sign In form
    Made some file refactoring and add a simple sign in form table.

--- chatApp.client/src/main.ts
+++ chatApp.client/src/main.ts
@@ -1,12 +1,12 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import { plugin, defaultConfig } from '@formkit/vue';
-import '@formkit/themes/genesis';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { createApp } from "vue";
+import "../src/styles/style.css";
+import App from "./App.vue";
+import { plugin, defaultConfig } from "@formkit/vue";
+import "@formkit/themes/genesis";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const app = createApp(App);
 
 app.use(plugin, defaultConfig);
 
-app.mount('#app')
+app.mount("#app");
